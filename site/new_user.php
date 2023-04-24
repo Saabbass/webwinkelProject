ea5dc4ffc1a18a2b264318a566c6912e9e6c262f
Saabbass <178414@novacollege.nl>
2023-04-24 12:48:30 +0200

    new_user code + css

--- site/new_user.php
+++ site/new_user.php
@@ -10,49 +10,83 @@
 </head>
 
 <body>
+    <!-- begin ingoegen van navbar / header -->
+    <?php
+    include('header.php');
+    ?>
+    <!-- einde invoegen van navbar / header -->
     <!-- action en method zijn atributen -->
-    <form action="session_new_user.php" method="post">
-        <h1>Maak een nieuwe gebruiker aan</h1>
-        <div class="form-group">
-            <!-- for="" is voor het drukken op de naam / label, dan wordt de input automatisch aangetikt -->
-            <label for="voornaam">Voornaam</label>
-            <input type="text" name="voornaam" id="voornaam">
+    <div class="container_img">
+        <div class="container">
+            <div class="container_width">
+                <section class="form_align">
+                    <form action="session_new_user.php" method="post">
+                        <h1 class="form_head">Maak een nieuwe gebruiker aan</h1>
+                        <div>
+                            <?php if (isset($_GET['error'])) { ?>
+                                <p class="error"><?php echo $_GET['error']; ?></p>
+                            <?php } ?>
+                        </div>
+                        <div>
+                            <?php if (isset($_GET['success'])) { ?>
+                                <p class="success"><?php echo $_GET['success']; ?></p>
+                            <?php } ?>
+                        </div>
+                        <div class="form_group">
+                            <!-- for="" is voor het drukken op de naam / label, dan wordt de input automatisch aangetikt -->
+                            <label for="voornaam">Voornaam</label>
+                            <input type="text" name="voornaam" id="voornaam">
+                        </div>
+                        <div class="form_group">
+                            <label for="achternaam">Achternaam</label>
+                            <input type="text" name="achternaam" id="achternaam">
+                        </div>
+                        <div class="form_group">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" id="email">
+                        </div>
+                        <div class="form_group">
+                            <label for="wachtwoord">Wachtwoord</label>
+                            <input type="password" name="wachtwoord" id="wachtwoord">
+                        </div>
+                        <div class="form_group">
+                            <label for="check_wachtwoord">Wachtwoord_check</label>
+                            <input type="password" name="check_wachtwoord" id="check_wachtwoord">
+                        </div>
+                        <div class="form_group">
+                            <label for="address">Address</label>
+                            <input type="text" name="address" id="address">
+                        </div>
+                        <div class="form_group">
+                            <label for="stad">Stad</label>
+                            <input type="text" name="stad" id="stad">
+                        </div>
+                        <div class="form_group_radio">
+                            <input type="radio" id="role1" name="role" value="administrator">
+                            <label for="role1">Administrator</label>
+                        </div>
+                        <div class="form_group_radio">
+                            <input type="radio" id="role2" name="role" value="employee">
+                            <label for="role2">Employee</label>
+                        </div>
+                        <div class="form_group_radio">
+                            <input type="radio" id="role3" name="role" value="customer">
+                            <label for="role3">Customer</label>
+                        </div>
+                        <div>
+                            <a href="" class="form_content_switch">Ik heb al een account</a>
+                            <button class="button_submit" type="sumbit">Maak nieuwe gebruiker</button>
+                        </div>
+                    </form>
+                </section>
+            </div>
+            <!-- begin footer -->
         </div>
-        <div class="form-group">
-            <label for="achternaam">Achternaam</label>
-            <input type="text" name="achternaam" id="achternaam">
-        </div>
-        <div class="form-group">
-            <label for="email">E-mail</label>
-            <input type="email" name="email" id="email">
-        </div>
-        <div class="form-group">
-            <label for="wachtword">Wachtword</label>
-            <input type="password" name="wachtword" id="wachtword">
-        </div>
-        <div class="form-group">
-            <label for="address">Address</label>
-            <input type="text" name="address" id="address">
-        </div>
-        <div class="form-group">
-            <label for="stad">Stad</label>
-            <input type="text" name="stad" id="stad">
-        </div>
-        <div class="form-group">
-            <input type="radio" id="role1" name="role" value="administrator">
-            <label for="role1">Administrator</label>
-        </div>
-        <div class="form-group">
-            <input type="radio" id="role2" name="role" value="employee">
-            <label for="role2">Employee</label>
-        </div>
-        <div class="form-group">
-            <input type="radio" id="role3" name="role" value="customer">
-            <label for="role3">Customer</label>
-        </div>
-        <button type="sumbit">Maak nieuwe gebruiker</button>
-    </form>
-
+    </div>
+    <?php
+    include('footer.php');
+    ?>
+    <!-- einde footer -->
 </body>
 
 </html>
--- site/session_new_user.php
+++ site/session_new_user.php
@@ -3,15 +3,15 @@
     $voornaam = $_POST['voornaam'];
     $achternaam = $_POST['achternaam'];
     $email = $_POST['email'];
-    $wachtword = $_POST['wachtword'];
+    $wachtwoord = $_POST['wachtwoord'];
     $address = $_POST['address'];
     $stad = $_POST['stad'];
     $role = $_POST['role'];
 
     require 'database.php';
 
-    $sql = "INSERT INTO users (firstname, lastname, email, password, address, city, role)
-    VALUES ('$voornaam', '$achternaam', '$email', '$wachtword', '$address', '$stad', '$role')";
+    $sql = "INSERT INTO gebruikers (firstname, lastname, email_address, password, address, city, role)
+    VALUES ('$voornaam', '$achternaam', '$email', '$wachtwoord', '$address', '$stad', '$role')";
 
     // Voer de INSERT INTO STATEMENT uit
     if(mysqli_query($conn, $sql)){
